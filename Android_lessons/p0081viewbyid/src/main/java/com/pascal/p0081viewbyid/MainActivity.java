68d53d268b17d75f83230449d23bfb4529909b9b
Andrew <shpuntik0902@gmail.com>
2020-12-16 16:20:35 +0300

    btn

--- Android_lessons/p0081viewbyid/src/main/java/com/pascal/p0081viewbyid/MainActivity.java
+++ Android_lessons/p0081viewbyid/src/main/java/com/pascal/p0081viewbyid/MainActivity.java
@@ -3,6 +3,7 @@ package com.pascal.p0081viewbyid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         TextView myTextView = (TextView) findViewById(R.id.myText);
+        myTextView.setText("New text in TextView");
+
+        Button myBtn = (Button)findViewById(R.id.myBtn);
+        myBtn.setText("My button");
+        myBtn.setEnabled(false);
     }
 }
